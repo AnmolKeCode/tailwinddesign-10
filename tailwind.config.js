4eede47d963750fd43e5eeffe29da457a9bebdf1
AnmolKeCode <130210804+AnmolKeCode@users.noreply.github.com>
2023-05-20 07:27:05 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1050px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
